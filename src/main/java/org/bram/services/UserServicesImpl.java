01f774ab8a0f796b9f77753ff753ed95a531a8a3
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-13 17:20:27 +0100

    new commit

--- src/main/java/org/bram/services/UserServicesImpl.java
+++ src/main/java/org/bram/services/UserServicesImpl.java
@@ -1,4 +1,20 @@
 package org.bram.services;
 
-public class UserServicesImpl {
+import org.bram.data.models.User;
+import org.bram.data.repositories.UserRepository;
+import org.bram.dtos.requests.RegisterUserRequest;
+import org.bram.dtos.response.RegisterUserResponse;
+
+public class UserServicesImpl implements UserServices {
+
+    private UserRepository userRepository;
+
+    public UserServicesImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public RegisterUserResponse register(RegisterUserRequest registerUserRequest) {
+        return null;
+    }
 }
---
+++ src/test/java/org/bram/services/UserServicesImplTest.java
@@ -0,0 +1,7 @@
+package org.bram.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServicesImplTest {
+
+}
