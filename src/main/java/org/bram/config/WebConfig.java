a7b6679bdd0d94cd5cd1e89364362658a4ee1432
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-20 10:40:15 +0100

    first commit

---
+++ src/main/java/org/bram/config/WebConfig.java
@@ -0,0 +1,18 @@
+package org.bram.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:63343")
+                .allowedHeaders("*")
+                .allowedMethods("POST", "GET", "PUT", "DELETE", "PATCH")
+                .allowCredentials(true);
+    }
+
+}
--- src/main/java/org/bram/controllers/TaskController.java
+++ src/main/java/org/bram/controllers/TaskController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping ("/tasks")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class TaskController {
 
     @Autowired
