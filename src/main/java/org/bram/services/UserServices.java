627b0136d89f1f7948ff7b69b9be79a68e2150d0
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-23 09:28:51 +0100

    first commit

--- src/main/java/org/bram/services/UserServices.java
+++ src/main/java/org/bram/services/UserServices.java
@@ -8,7 +8,10 @@ public interface UserServices {
     RegisterUserResponse registerUser(RegisterUserRequest request);
 
     LoginResponse login(LoginRequest loginRequest);
+
     ChangePasswordResponse changePassword(ChangePasswordRequest request);
+
     ChangeEmailResponse changeEmail(ChangeEmailRequest request);
+
     UserLogoutResponse logout(UserLogoutRequest request);
 }
