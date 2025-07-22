bd44f6b94941fd194bcfc244cd3d3cc87c6d2447
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-22 15:59:29 +0100

    new

--- src/main/java/org/bram/Main.java
+++ src/main/java/org/bram/Main.java
@@ -3,7 +3,6 @@ package org.bram;
 import me.paulschwarz.springdotenv.DotenvPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import me.paulschwarz.springdotenv.DotenvPropertySource;
 
 @SpringBootApplication
 public class Main {
