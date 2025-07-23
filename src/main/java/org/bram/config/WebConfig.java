f37a59e06411328c4b3ee6e29502c3d8fd00beb2
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-23 21:41:26 +0100

    new

--- src/main/java/org/bram/config/WebConfig.java
+++ src/main/java/org/bram/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:63343")
+                .allowedOrigins("http://127.0.0.1:5500")
                 .allowedHeaders("*")
                 .allowedMethods("POST", "GET", "PUT", "DELETE", "PATCH")
                 .allowCredentials(true);
