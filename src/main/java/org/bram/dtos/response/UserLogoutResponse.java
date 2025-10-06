d6dbe5fc839ac57a55111cddc0ac9eec62d4f93b
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-06 13:52:56 +0100

    update user service

--- src/main/java/org/bram/dtos/response/UserLogoutResponse.java
+++ src/main/java/org/bram/dtos/response/UserLogoutResponse.java
@@ -1,8 +1,12 @@
 package org.bram.dtos.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class UserLogoutResponse {
 
     private String message;
