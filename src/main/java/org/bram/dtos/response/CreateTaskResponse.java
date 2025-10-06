54278018080f7027ea994afe41208570155271c9
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-06 14:22:18 +0100

    update user service

--- src/main/java/org/bram/dtos/response/CreateTaskResponse.java
+++ src/main/java/org/bram/dtos/response/CreateTaskResponse.java
@@ -1,6 +1,8 @@
 package org.bram.dtos.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.bram.data.models.TaskStatus;
 import org.springframework.data.annotation.Id;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -8,6 +10,8 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class CreateTaskResponse {
 
     @Id
